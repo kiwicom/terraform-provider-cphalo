2d9872662a872863dbc47a272c458e5eaf9918b4
Dusan Simek <dusan.simek@kiwi.com>
2018-08-01 14:24:35 +0200

    Add provider schema

--- cphalo/provider.go
+++ cphalo/provider.go
@@ -5,5 +5,36 @@ import (
 )
 
 func Provider() *schema.Provider {
-	return nil
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"endpoint": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CP_ENDPOINT", "https://api.cloudpassage.com/v1"),
+				Description: descriptions["endpoint"],
+			},
+			"application_key": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CP_APPLICATION_KEY", nil),
+				Description: descriptions["application_key"],
+			},
+			"application_secret": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CP_APPLICATION_SECRET", nil),
+				Description: descriptions["application_secret"],
+			},
+		},
+	}
+}
+
+var descriptions map[string]string
+
+func init() {
+	descriptions = map[string]string{
+		"endpoint":           "The CP API endpoint url, default to https://api.cloudpassage.com/v1",
+		"application_key":    "The CP API application key",
+		"application_secret": "The CP API application secret",
+	}
 }
