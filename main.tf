5d50a87ee0fd7ce47021ba1bef38a3e9f73a8962
Matej Velikonja <matej@velikonja.si>
2018-11-21 13:30:22 +0100

    added another example of server group

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "cphalo_server_group" "root_group" {
   "name" = "kiwi.com"
 }
 
-output "root_group_id" {
+output "server_group_root_id" {
   value = "${data.cphalo_server_group.root_group.id}"
 }
 
@@ -12,25 +12,34 @@ resource "cphalo_server_group" "main" {
   name = "tf-main"
 }
 
+output "server_group_main_id" {
+  value = "${cphalo_server_group.main.id}"
+}
+
+resource "cphalo_server_group" "main_parent" {
+  name = "tf-main-parent"
+  parent_id = "${data.cphalo_server_group.root_group.id}"
+}
+
+output "server_group_main_parent_id" {
+  value = "${cphalo_server_group.main_parent.id}"
+}
+
 resource "cphalo_server_group" "sg1" {
   name = "tf-group"
   parent_id = "${cphalo_server_group.main.id}"
 }
 
-output "main_group_id" {
-  value = "${cphalo_server_group.main.id}"
-}
-
-output "created_group_id" {
+output "server_group_sg1_id" {
   value = "${cphalo_server_group.sg1.id}"
 }
 
 resource "cphalo_server_group" "sg2" {
-  name = "tf-group44"
+  name = "tf-group2"
   parent_id = "${cphalo_server_group.main.id}"
   tag = "new_tag"
 }
 
-output "created_sub_group_id" {
+output "server_group_sg2_id" {
   value = "${cphalo_server_group.sg2.id}"
 }
