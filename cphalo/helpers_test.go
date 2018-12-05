c6d8f71429433a8b49983980f83d3dac074f422d
Matej Velikonja <matej@velikonja.si>
2018-12-05 10:01:21 +0100

    expandStringList test

--- cphalo/helpers_test.go
+++ cphalo/helpers_test.go
@@ -1,6 +1,7 @@
 package cphalo
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 )
@@ -23,6 +24,38 @@ func Test_AssertStringSlice(t *testing.T) {
 		t.Run(fmt.Sprintf("%.2d", i), func(t *testing.T) {
 			if tt.same != assertStringSlice(tt.a1, tt.a2) {
 				t.Errorf("expected %v=%v to result in %t", tt.a1, tt.a2, tt.same)
+			}
+		})
+	}
+}
+
+func Test_ExpandStringList(t *testing.T) {
+	tests := []struct {
+		s []string
+	}{
+		{[]string{}},
+		{[]string{"1"}},
+		{[]string{"1", "2", "3"}},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%.2d", i), func(t *testing.T) {
+			s := tt.s
+			var i interface{}
+
+			b, err := json.Marshal(s)
+			if err != nil {
+				t.Fatalf("marhsalling failed: %v", err)
+			}
+
+			if err := json.Unmarshal(b, &i); err != nil {
+				t.Fatalf("unmarhsalling failed: %v", err)
+			}
+
+			got := expandStringList(i)
+
+			if !assertStringSlice(s, got) {
+				t.Errorf("expected %v; got %v", s, got)
 			}
 		})
 	}
