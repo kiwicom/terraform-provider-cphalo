50cc4b32c1de78a15a6adbb87de94f06ef56d798
Matej Velikonja <matej@velikonja.si>
2018-12-18 09:55:19 +0100

    log helper with printf funcs

--- cphalo/logging.go
+++ cphalo/logging.go
@@ -9,22 +9,46 @@ func logWithLevel(level string, v ...interface{}) {
 	log.Print(fmt.Sprintf("[%s] ", level), fmt.Sprint(v...))
 }
 
+func logWithLevelf(level, format string, v ...interface{}) {
+	log.Printf(fmt.Sprintf("[%s] %s", level, format), v...)
+}
+
 func logTrace(v ...interface{}) {
 	logWithLevel("TRACE", v...)
+}
+
+func logTracef(format string, v ...interface{}) {
+	logWithLevelf("TRACE", format, v...)
 }
 
 func logDebug(v ...interface{}) {
 	logWithLevel("DEBUG", v...)
 }
 
+func logDebugf(format string, v ...interface{}) {
+	logWithLevelf("DEBUG", format, v...)
+}
+
 func logInfo(v ...interface{}) {
 	logWithLevel("INFO", v...)
+}
+
+func logInfof(format string, v ...interface{}) {
+	logWithLevelf("INFO", format, v...)
 }
 
 func logWarn(v ...interface{}) {
 	logWithLevel("WARN", v...)
 }
 
+func logWarnf(format string, v ...interface{}) {
+	logWithLevelf("WARN", format, v...)
+}
+
 func logError(v ...interface{}) {
 	logWithLevel("ERROR", v...)
+}
+
+func logErrorf(format string, v ...interface{}) {
+	logWithLevelf("ERROR", format, v...)
 }
--- cphalo/logging_test.go
+++ cphalo/logging_test.go
@@ -37,7 +37,41 @@ func TestLogging(t *testing.T) {
 			expected := fmt.Sprintf("[%s] %s", test.lvl, msg)
 
 			if !strings.Contains(got, expected) {
-				t.Errorf("expected %s to be substring of %s", expected, got)
+				t.Errorf("expected `%s` to be substring of `%s`", expected, got)
+			}
+		})
+	}
+}
+
+func TestLoggingF(t *testing.T) {
+	tt := []struct {
+		f   func(format string, v ...interface{})
+		lvl string
+	}{
+		{logTracef, "TRACE"},
+		{logDebugf, "DEBUG"},
+		{logInfof, "INFO"},
+		{logWarnf, "WARN"},
+		{logErrorf, "ERROR"},
+	}
+
+	for _, test := range tt {
+		t.Run(test.lvl, func(t *testing.T) {
+			var buf bytes.Buffer
+			log.SetOutput(&buf)
+			defer func() {
+				log.SetOutput(os.Stderr)
+			}()
+
+			format := "test format %d"
+			test.f(format, 1)
+
+			got := buf.String()
+
+			expected := fmt.Sprintf("[%s] %s", test.lvl, fmt.Sprintf("test format %d", 1))
+
+			if !strings.Contains(got, expected) {
+				t.Errorf("expected `%s` to be substring of `%s`", expected, got)
 			}
 		})
 	}
