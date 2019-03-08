84a0165af54b53ff4b210e350ea539061d3f1aa9
Jan Gorjanc <johnee@gmail.com>
2019-03-08 08:37:50 +0100

    Add cphalo_server_group_firewall_policy resource

---
+++ cphalo/testdata/server_group_firewall_policies/basic_02.tf
@@ -0,0 +1,20 @@
+resource "cphalo_server_group" "root_group" {
+  name = "{{.Prefix}}root group"
+}
+
+resource "cphalo_firewall_policy" "firewall_policy" {
+  name   = "{{.Prefix}}tf_acc_fw_policy"
+  shared = true
+
+  rule {
+    chain             = "INPUT"
+    action            = "ACCEPT"
+    connection_states = "ANY"
+    position          = 1
+
+    firewall_source {
+      id   = "${cphalo_server_group.root_group.id}"
+      kind = "Group"
+    }
+  }
+}
